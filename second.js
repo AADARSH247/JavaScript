053a71e8146e2746b7b10ec6ba36bfde24f43de3
AADARSH247 <adarshghimire3@gmail.com>
2025-12-22 11:03:45 +0545

    Second commit

---
+++ second.js
@@ -0,0 +1,48 @@
+// console.log("Hello World!")
+// let a=2
+// let b=3
+
+// console.log("a + b =",a+b)
+// console.log("a - b =",a-b)
+// console.log("a * b =",a*b)
+// console.log("a / b =",a/b)
+// console.log("a % b =",a%b)
+// console.log("a power b =",a**b)
+
+// //console.log("a++",a++)//2
+// //console.log("a",a)
+
+// console.log("a==b",a==b)
+
+// let c=4;
+// let d="4";//string is converted in number here 
+
+// console.log("a==b",c==d);
+// console.log("a===b",a===b)
+ 
+
+// let age=20;
+// if(age>20){
+//     console.log("You Can Vote");
+
+// }
+// if(age<20){
+//     console.log("you cannot vote");
+// }
+// let mode="dark";
+// let color;
+// if(mode=="dark"){
+//     color="black";
+// }
+// if(mode=="light"){
+//     color="white";
+// }
+// console.log(color)
+let number=prompt("Enter a number:");
+if (number%5==0){
+    console.log("Multiple of 5");
+}
+else{
+    console.log("Not a multiple of 5");
+}
+console.log("Hello!")
