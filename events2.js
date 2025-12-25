11596824471c002ebf1df3827bf33840dbb854d3
AADARSH247 <adarshghimire3@gmail.com>
2025-12-25 17:54:09 +0545

    push

---
+++ events2.js
@@ -0,0 +1,5 @@
+ const btn=document.getElementById("buttonid");
+btn.addEventListener("mouseup",(evt)=>{ // this mouseup fires when you release the mouse button on the button
+      console.log("You hovered over the button...");
+      console.log("This button was activated when you released the mouse click on this button..");
+ });
--- lightmode.js
+++ lightmode.js
@@ -1,16 +1,31 @@
-let modechange = document.querySelector("#modechange");
-let body = document.querySelector("body");
-let currmode="light";
-modechange.addEventListener("click",()=>{
-    console.log("You are trying to change the theme");
-    if(currmode==="light"){
-        currmode="dark";
-        body.classList.remove("light");
-        body.classList.add("dark");
-    }else{
-        currmode="light";
-        body.classList.remove("dark");
-        body.classList.add("light");
-    }
-    console.log(currmode);
-});
+// let modechange = document.querySelector("#modechange");
+// let body = document.querySelector("body");
+// let currmode="light";
+// modechange.addEventListener("click",()=>{
+//     console.log("You are trying to change the theme");
+//     if(currmode==="light"){
+//         currmode="dark";
+//         body.classList.remove("light");
+//         body.classList.add("dark");
+//     }else{
+//         currmode="light";
+//         body.classList.remove("dark");
+//         body.classList.add("light");
+//     }
+//     console.log(currmode);
+// });
+function simulateClick() {
+  // Get the element to send a click event
+  const cb = document.getElementById("checkbox");
+
+  // Create a synthetic click MouseEvent
+  let evt = new MouseEvent("click", {
+    bubbles: true,
+    cancelable: true,
+    view: window,
+  });
+
+  // Send the event to the checkbox element
+  cb.dispatchEvent(evt);
+}
+document.getElementById("button").addEventListener("click", simulateClick);
